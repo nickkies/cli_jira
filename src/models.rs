9dd5a6db5313927f73b1f99cd5692db4e66e0008
YoonSung Kim <opqudk@gmail.com>
2023-12-15 17:10:10 +0900

    ✨ feat: add action enum (#3)

--- src/models.rs
+++ src/models.rs
@@ -2,6 +2,19 @@ use std::collections::HashMap;
 
 use serde::{Deserialize, Serialize};
 
+pub enum Action {
+    NavigateToEpicDetail { epic_id: u32 },
+    NavigateToStoryDetail { epic_id: u32, story_id: u32 },
+    NavigateToPreviousPage,
+    CreateEpic,
+    UpdateEpicStatus { epic_id: u32 },
+    DeleteEpic { epic_id: u32 },
+    CreateStory { epic_id: u32 },
+    UpdateStoryStatus { story_id: u32 },
+    DeleteStory { epic_id: u32, story_id: u32 },
+    Exit,
+}
+
 #[derive(PartialEq, Eq, Debug, Serialize, Deserialize, Clone)]
 pub enum Status {
     Open,
