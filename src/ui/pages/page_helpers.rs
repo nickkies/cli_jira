271f23f26603482f135540864ad2a4249e5dae95
YoonSung Kim <opqudk@gmail.com>
2023-12-15 17:04:33 +0900

    ✨ feat: add get_column_string (#3)

--- src/ui/pages/page_helpers.rs
+++ src/ui/pages/page_helpers.rs
@@ -1,5 +1,38 @@
+use std::cmp::Ordering::{Equal, Greater, Less};
+
+use ellipse::Ellipse;
+
 pub fn get_column_string(text: &str, width: usize) -> String {
-    todo!()
+    let len = text.len();
+
+    match len.cmp(&width) {
+        Equal => text.to_string(),
+        Less => {
+            let left_over = width - len;
+            let mut column_string = text.to_string();
+            let padding = std::iter::repeat(' ').take(left_over).collect::<String>();
+
+            column_string.push_str(&padding);
+
+            column_string
+        }
+        Greater => {
+            let result;
+            if width == 0 {
+                result = "".to_string();
+            } else if width == 1 {
+                result = ".".to_string();
+            } else if width == 2 {
+                result = "..".to_string();
+            } else if width == 3 {
+                result = "...".to_string();
+            } else {
+                result = text.truncate_ellipse(width - 3).to_string();
+            }
+
+            result
+        }
+    }
 }
 
 #[cfg(test)]
