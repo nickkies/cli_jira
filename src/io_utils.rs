eefb4ca0393acbdfeb80e8221ef8ac6dcd6ef699
YoonSung Kim <opqudk@gmail.com>
2023-12-19 09:41:55 +0900

    👔 feat: add io util fn (#4)

--- src/io_utils.rs
+++ src/io_utils.rs
@@ -1,10 +1,16 @@
+use std::io;
+
 pub fn get_user_input() -> String {
     let mut user_input = String::new();
-    std::io::stdin().read_line(&mut user_input).unwrap();
+    io::stdin().read_line(&mut user_input).unwrap();
 
     user_input
 }
 
 pub fn get_user_input_trimmed() -> String {
     get_user_input().trim().to_string()
+}
+
+pub fn wait_for_key_press() {
+    io::stdin().read_line(&mut String::new()).unwrap();
 }
